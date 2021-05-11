1358f4a82dfab94675f55fda42b9f575018b158f
MikhailZarif <mishka.suren@gmail.com>
2021-05-11 23:04:25 +0300

    files

---
+++ solver_lib/_build/CMakeFiles/Solver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solver.dir/home/enenra/lab03/lab03/solver_application/equation.cpp.o"
+  "Solver"
+  "Solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ solver_lib/_build/CMakeFiles/solver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solver.dir/home/enenra/lab03/lab03/formatter_ex_lib/formatter_ex.cpp.o"
+  "CMakeFiles/solver.dir/home/enenra/lab03/lab03/formatter_lib/formatter.cpp.o"
+  "CMakeFiles/solver.dir/solver.cpp.o"
+  "libsolver.a"
+  "libsolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
